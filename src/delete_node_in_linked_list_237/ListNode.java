57b302c3d6452b8d971c66d5fd17b48bc8dbcfd0
Zhicong Huang <zhihuang@icsil1noteb176.local>
2015-12-31 18:52:14 +0100

    237 delete node in linked list

---
+++ src/delete_node_in_linked_list_237/ListNode.java
@@ -0,0 +1,8 @@
+package delete_node_in_linked_list_237;
+
+
+public class ListNode {
+	int val;
+ 	ListNode next;
+ 	ListNode(int x) { val = x; }
+}
---
+++ src/delete_node_in_linked_list_237/Solution.java
@@ -0,0 +1,20 @@
+package delete_node_in_linked_list_237;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void deleteNode(ListNode node) {
+        if(node.next == null){
+        	node = null;
+        	return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
