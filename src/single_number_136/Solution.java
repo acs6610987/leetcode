3e7f589c3728c2eaee772b5df90177952e5ea399
Zhicong Huang <zhihuang@icsil1noteb176.local>
2015-12-31 11:41:35 +0100

    136 single number without extra space

--- src/single_number_136/Solution.java
+++ src/single_number_136/Solution.java
@@ -2,5 +2,10 @@ package single_number_136;
 
 public class Solution {
     public int singleNumber(int[] nums) {
-        
+        if(nums.length == 1) return nums[0];
+        nums[1] = nums[1] ^ nums[0];
+        for(nums[0] = 2; nums[0] < nums.length; nums[0]++)
+        	nums[nums[0]] = nums[nums[0]] ^ nums[nums[0] - 1];
+        return nums[nums.length - 1];
     }
+}
