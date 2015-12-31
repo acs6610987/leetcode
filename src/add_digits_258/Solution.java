7464a73ab221ad103dfb9104df796e0fddd1b290
Zhicong Huang <zhihuang@icsil1noteb176.local>
2015-12-31 17:53:03 +0100

    258 add digits

---
+++ src/add_digits_258/Solution.java
@@ -0,0 +1,11 @@
+package add_digits_258;
+
+/*
+ * Because (10a+b)%9 == (a+b)%9
+ */
+public class Solution {
+    public int addDigits(int num) {
+    	if(num == 0) return 0;
+        return num%9 == 0? 9 : num%9;
+    }
+}
