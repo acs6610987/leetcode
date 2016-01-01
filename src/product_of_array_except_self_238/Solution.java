cb336b870724d04bca288f8ddab30ed517f2638b
Zhicong Huang <zhihuang@icsil1noteb176.local>
2016-01-01 14:03:05 +0100

    238 product of array except self

---
+++ src/product_of_array_except_self_238/Solution.java
@@ -0,0 +1,16 @@
+package product_of_array_except_self_238;
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if(nums.length == 0) return new int[]{};
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        for(int i = 1; i < nums.length; i++)
+        	output[i] = output[i-1] * nums[i-1];
+        for(int i = nums.length - 2; i >= 0; i--)
+        	nums[i] = nums[i] * nums[i+1];
+        for(int i = 0; i < nums.length - 1; i++)
+        	output[i] = output[i] * nums[i+1];
+        return output;
+    }
+}
---
+++ src/product_of_array_except_self_238/Test.java
@@ -0,0 +1,10 @@
+package product_of_array_except_self_238;
+
+public class Test {
+	public static void main(String[] args){
+		int[] nums = new int[]{1,2,3,4};
+		int[] output = new Solution().productExceptSelf(nums);
+		for(int i = 0; i < output.length; i++)
+			System.out.print(output[i] + ", ");
+	}
+}
