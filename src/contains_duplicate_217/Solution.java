b6c9e4ff677f7773ec18a95c4633f39b7c40c291
Zhicong Huang <zhihuang@icsil1noteb176.local>
2016-01-01 17:01:17 +0100

    217 contains duplicate

---
+++ src/contains_duplicate_217/Solution.java
@@ -0,0 +1,15 @@
+package contains_duplicate_217;
+
+import java.util.Hashtable;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Hashtable<Integer, Integer> numCount = new Hashtable<>();
+        for(int i = 0; i < nums.length; i++){
+        	if(numCount.containsKey(nums[i]))
+        		return true;
+        	numCount.put(nums[i], 1);
+        }
+        return false;
+    }
+}
