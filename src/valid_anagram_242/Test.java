b59d3351717011b39e04328c2ef38f5cad482f74
Zhicong Huang <zhihuang@icsil1noteb176.local>
2016-01-01 14:16:20 +0100

    242 valid anagram

---
+++ src/valid_anagram_242/Test.java
@@ -0,0 +1,8 @@
+package valid_anagram_242;
+
+public class Test {
+	public static void main(String[] args){
+		System.out.println(new Solution().isAnagram("anagram", "nagaram"));
+		System.out.println(new Solution().isAnagram("rat", "car"));
+	}
+}
