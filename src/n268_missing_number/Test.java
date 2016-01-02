6d17a49d969d2768f1d561ee92ed3827a23ef13f
Zhicong Huang <zhihuang@icsil1noteb176.local>
2016-01-02 12:00:58 +0100

    268 missing number

---
+++ src/n268_missing_number/Test.java
@@ -0,0 +1,8 @@
+package n268_missing_number;
+
+public class Test {
+	public static void main(String[] args){
+		int[] nums = new int[]{0, 1, 3};
+		System.out.println(new Solution().missingNumber(nums));
+	}
+}
