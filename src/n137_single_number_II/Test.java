72270b3f3c573c53a094944bdbea5850fb9cb3a3
Zhicong Huang <zhihuang@icsil1noteb176.epfl.ch>
2016-01-04 14:26:43 +0100

    137 single number II

---
+++ src/n137_single_number_II/Test.java
@@ -0,0 +1,8 @@
+package n137_single_number_II;
+
+public class Test {
+	public static void main(String[] args){
+		int[] nums = new int[]{1, 2, 4, 4, 5, 4, 2, 2, 1, 5, 3, 5, 1};
+		System.out.println(new Solution().singleNumber(nums));
+	}
+}
