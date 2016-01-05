f0700ac87bef056a5779a375ec745fe4cf9f8546
Zhicong Huang <zhihuang@icsil1noteb176.epfl.ch>
2016-01-05 09:46:25 +0100

    263 ugly number

---
+++ src/n263_ugly_number/Solution.java
@@ -0,0 +1,11 @@
+package n263_ugly_number;
+
+public class Solution {
+    public boolean isUgly(int num) {
+    	if(num <= 0) return false;
+        while(num % 2 == 0) num >>= 1;
+        while(num % 3 == 0) num /= 3;
+        while(num % 5 == 0) num /= 5;
+        return num == 1;
+    }
+}
---
+++ src/n263_ugly_number/Test.java
@@ -0,0 +1,8 @@
+package n263_ugly_number;
+
+public class Test {
+	public static void main(String[] args){
+		System.out.println(new Solution().isUgly(6));
+		System.out.println(new Solution().isUgly(14));
+	}
+}
