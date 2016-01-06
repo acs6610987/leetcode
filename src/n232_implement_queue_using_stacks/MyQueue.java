a76984e045b4397c8e0be30f7f742254c3636167
Zhicong Huang <zhihuang@icsil1noteb176.local>
2016-01-06 23:15:37 +0100

    232 implement queue using stacks

---
+++ src/n232_implement_queue_using_stacks/MyQueue.java
@@ -0,0 +1,35 @@
+package n232_implement_queue_using_stacks;
+
+import java.util.Stack;
+
+public class MyQueue {
+	Stack<Integer> pushStack = new Stack<>();
+	Stack<Integer> popStack = new Stack<>();
+    // Push element x to the back of queue.
+    public void push(int x) {
+        pushStack.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        if(popStack.isEmpty()){
+        	while(!pushStack.isEmpty())
+        		popStack.push(pushStack.pop());
+        }
+        popStack.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+    	if(popStack.isEmpty()){
+        	while(!pushStack.isEmpty())
+        		popStack.push(pushStack.pop());
+        }
+        return popStack.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return pushStack.isEmpty() && popStack.isEmpty();
+    }
+}
