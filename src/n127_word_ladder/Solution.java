aac5749bfe0afca064d705f03a57988b2ca20eb7
Zhicong Huang <zhihuang@icsil1noteb176.local>
2016-01-12 22:26:33 +0100

    127 word ladder

---
+++ src/n127_word_ladder/Solution.java
@@ -0,0 +1,46 @@
+package n127_word_ladder;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+    	int distance = 1;
+    	Queue<String> bfsQueue = new LinkedList<>();
+    	bfsQueue.add(beginWord);
+    	while(!bfsQueue.isEmpty()){
+    		distance++;
+    		int currentSize = bfsQueue.size();
+    		for(int i = 0; i < currentSize; i++){
+    			String word = bfsQueue.poll();
+    			StringBuilder adjacentWord = new StringBuilder(word);
+    			for(int j = 0; j < word.length(); j++){
+    				for(int k = 0; k < 26; k++){
+    					if('a'+k == (int)word.charAt(j)) continue;
+    					adjacentWord.setCharAt(j, (char)('a'+k));
+    					if(adjacentWord.toString().equals(endWord))
+    						return distance;
+    					if(wordList.contains(adjacentWord.toString())){
+    						wordList.remove(adjacentWord.toString());
+    						bfsQueue.add(adjacentWord.toString());
+    					}
+    				}
+    				adjacentWord.setCharAt(j, word.charAt(j));
+    			}
+    		}
+    	}
+    	return 0;
+    }
+    
+//    private boolean isAdjacent(String a, String b){
+//    	int distance = 0;
+//    	for(int i = 0; i < a.length(); i++){
+//    		if(a.charAt(i) != b.charAt(i)){
+//    			distance++;
+//    			if(distance >= 2) return false;
+//    		}
+//    	}
+//    	return distance == 1;
+//    }
+}
